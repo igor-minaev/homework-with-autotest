7c2fa8a599fd73139d4bb1796d6b70310bbc5957
Igor Minayeu <igor.minayeu@gmail.com>
2023-04-01 17:26:33 +0300

    homework 8

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -4,15 +4,18 @@ type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
         case 'sort': { // by name
-
-            return state // need to fix
+            return action.payload === 'up'
+                ? [...state].sort((a, b) => a.name.localeCompare(b.name))
+                : action.payload === 'down'
+                    ? [...state].sort((a, b) => b.name.localeCompare(a.name))
+                    : [...state]// need to fix
         }
         case 'check': {
 
-            return state // need to fix
+            return state.filter(u => u.age >= action.payload)
         }
         default:
             return state
